6165965f45f0fcdacc8f480d0678aad3e81252f8
Maarten De Wilde <maarten.dewilde@gmail.com>
2021-02-02 11:32:36 +0100

    Add settings files, wire up settings for mysql dbcontext.

---
+++ Logic/Db/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Db
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseMySQL(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ Logic/Model/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Model
+{
+    public class Invoice
+    {
+        public string Number { get; set; }
+
+        public string Customer { get; set; }
+
+        public virtual ICollection<InvoiceItem> Items { get; set; }
+
+    }
+
+    public class InvoiceItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
